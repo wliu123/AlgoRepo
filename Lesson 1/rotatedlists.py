434c1af68a0e2492809f814eede3b0b1b2eb5dab
William Liu <williamliu626@gmail.com>
2023-11-26 18:31:06 -0700

    create tests fo r bonus

--- Lesson 1/rotatedlists.py
+++ Lesson 1/rotatedlists.py
@@ -69,3 +69,44 @@ def evaluate_test_cases(function, tests):
         print(f"Actual Result: {result}")
         print(result == test['output'])
 evaluate_test_cases(count_rotations, tests)
+
+tests.clear()
+
+#Bonus Question
+
+# Search in rotated sorted array:
+# There is an integer array nums sorted in ascending order (with distinct values).
+
+# Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+
+# Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+
+# You must write an algorithm with O(log n) runtime complexity.
+
+tests.append({'input': {
+        'nums':[4,5,6,7,0,1,2],
+        'target':0
+    },
+    'output':4
+})
+tests.append({'input': {
+        'nums':[4,5,6,7,0,1,2],
+        'target':-1
+    },
+    'output':-1
+})
+tests.append({'input': {
+        'nums':[1],
+        'target':0
+    },
+    'output':-1
+})
+tests.append({'input': {
+        'nums':[1],
+        'target':1
+    },
+    'output':0
+})
+
+def count_rotate_sort(nums, target):
+    pass
