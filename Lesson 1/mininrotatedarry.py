d9f3ef307fec71c66eabff7acc73ad7633010667
William Liu <williamliu626@gmail.com>
2023-12-03 16:23:38 -0500

    generate test caseSS

--- Lesson 1/mininrotatedarry.py
+++ Lesson 1/mininrotatedarry.py
@@ -7,3 +7,48 @@
 # Given the sorted rotated array nums of unique elements, return the minimum element of this array.
 
 # You must write an algorithm that runs in O(log n) time.
+
+# Own words: Given an array that is sorted and has been rotated a certain number of times, find the lowest value in the array.
+
+# Test cases
+tests = []
+
+tests.append({
+    'input': {
+        'nums':[3, 4, 5, 1, 2]
+    },
+    'output': 1
+})
+
+tests.append({
+    'input': {
+        'nums':[4, 5, 6, 7, 0, 1, 2]
+    },
+    'output': 0
+})
+
+tests.append({
+    'input': {
+        'nums':[11, 13, 15, 17]
+    },
+    'output': 11
+})
+
+tests.append({
+    'input': {
+        'nums':[-2, 1, 3, 7, -12, -4, -3]
+    },
+    'output': -12
+})
+
+tests.append({
+    'input': {
+        'nums':[3]
+    },
+    'output': 3
+})
+
+def findMin(nums):
+    if len(nums) == 1: return nums[0]
+    low, high = 0, len(nums) - 1
+    
