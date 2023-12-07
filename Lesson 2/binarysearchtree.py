d65cec57aaee4642eecf62a0dfa657630a9edd35
William Liu <williamliu626@gmail.com>
2023-12-06 22:34:49 -0500

    crafted tree

---
+++ Lesson 2/binarysearchtree.py
@@ -0,0 +1,40 @@
+# Exercise: Define a function tree_to_tuple that converts a binary tree into a tuple representing the same tree. E.g. tree_to_tuple converts the tree created above to the tuple ((1, 3, None), 2, ((None, 3, 4), 5, (6, 7, 8))). 
+
+class TreeNode:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
+def parse_tuple(data):
+    # print(data)
+    if isinstance(data, tuple) and len(data) == 3:
+        node = TreeNode(data[1])
+        node.left = parse_tuple(data[0])
+        node.right = parse_tuple(data[2])
+    elif data is None:
+        node = None
+    else:
+        node = TreeNode(data)
+    return node
+
+tree = parse_tuple(((1, 3, None), 2, ((None, 3, 4), 5, (6, 7, 8))))
+def trees_to_tuple(node):
+    pass
+
+def display_keys(node, space='\t', level=0):
+    # print(node.key if node else None, level)
+
+    if node is None:
+        print(space*level + '∅')
+        return
+    
+    if node.left is None and node.right is None:
+        print(space*level + str(node.key))
+        return
+    
+    display_keys(node.right, space, level+1)
+    print(space*level, str(node.key))
+    display_keys(node.left, space, level+1)
+
+display_keys(tree, '  ')
